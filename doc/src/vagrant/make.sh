a6b5f40e3db3a770d6bb40dec79af9fe9e4c4d84
Hans Petter Langtangen <hpl@simula.no>
2013-08-10 21:54:29 +0200

    updates

--- doc/src/vagrant/make.sh
+++ doc/src/vagrant/make.sh
@@ -1,26 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 set -x
+
+function system {
+  "$@"
+  if [ $? -ne 0 ]; then
+    echo "Unsuccessful run of $@"
+    exit 1
+  fi
+}
+
 
 # Build various formats
 name=vagrant_box
+system doconce spellcheck -d .dict4spell $name.do.txt
+system doconce format html $name --html_style=vagrant --html_template=template.html
+system doconce split_html $name
 
-doconce format html $name --html_style=vagrant --html_template=template.html
-doconce split_html $name
-exit
+# Use title and author
+name2=main_${name}
+system doconce format pdflatex $name2 DEVICE=paper
+system doconce ptex2tex $name2 envir=minted
+pdflatex -shell-escape $name2
+pdflatex -shell-escape $name2
 
-doconce format pdflatex $name
-doconce ptex2tex $name envir=minted
-pdflatex -shell-escape $name
-pdflatex -shell-escape $name
+#doconce sphinx_dir $name theme=agni
+system doconce sphinx_dir $name2 theme=cbc
+system python automake_sphinx.py
 
-doconce sphinx_dir $name theme=agni
-python automake-sphinx.py
-
-if [ -d pub ]; then
-  rm -rf pub
-fi
-mkdir pub
-cp -r $name.html $name.pdf sphinx-rootdir/_build/html pub
-cd pub
-mv html sphinx
+dest=../../pub
+rm -rf $dest/sphinx
+cp -r _static $name.html ._part*$name.html $name2.pdf sphinx-rootdir/_build/html $dest
+cd $dest
+mv -f html sphinx
 
