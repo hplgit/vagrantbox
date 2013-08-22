2ba08887d0846876f184bca5e8a2a86a588763a1
Hans Petter Langtangen <hpl@simula.no>
2013-08-21 18:59:43 -0700

    updates

--- doc/src/vagrant/make_course.sh
+++ doc/src/vagrant/make_course.sh
@@ -3,7 +3,7 @@ name=vagrant_course
 doconce spellcheck -d .dict4spell $name.do.txt
 if [ $? -ne 0 ]; then exit; fi
 
-doconce format html $name --html_style=bloodish COURSE=INF1100 --html_output=vagrant_inf1100.html
+doconce format html $name --html_style=bloodish COURSE=INF1100 --html_output=vagrant_inf1100
 scp vagrant_inf1100.html inf1100@login.ifi.uio.no:www_docs/
 
-doconce format html $name --html_style=bloodish COURSE=INF5620 --html_output=vagrant_inf5620.html
+doconce format html $name --html_style=bloodish COURSE=INF5620 --html_output=vagrant_inf5620
