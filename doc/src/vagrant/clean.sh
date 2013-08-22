4080cd1f9fe87145cacee030474845a35ea82c16
Hans Petter Langtangen <hpl@simula.no>
2013-08-21 18:57:35 -0700

    updates

--- doc/src/vagrant/clean.sh
+++ doc/src/vagrant/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 doconce clean
-rm -rf automake_* latex_figs *-rootdir*
+rm -rf automake_* latex_figs *-rootdir* vagrant_inf*.html
---
+++ doc/src/vagrant/make_course.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+name=vagrant_course
+doconce spellcheck -d .dict4spell $name.do.txt
+if [ $? -ne 0 ]; then exit; fi
+
+doconce format html $name --html_style=bloodish COURSE=INF1100 --html_output=vagrant_inf1100.html
+scp vagrant_inf1100.html inf1100@login.ifi.uio.no:www_docs/
+
+doconce format html $name --html_style=bloodish COURSE=INF5620 --html_output=vagrant_inf5620.html
