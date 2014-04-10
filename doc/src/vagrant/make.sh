ba7ccb7fe222beb5d23e4a0b9873a16c0b483cd9
Hans Petter Langtangen <hpl@simula.no>
2014-04-10 08:42:08 +0200

    updates

--- doc/src/vagrant/make.sh
+++ doc/src/vagrant/make.sh
@@ -35,7 +35,7 @@ system python automake_sphinx.py $opts
 # Install in pub directory
 dest=../../pub
 rm -rf $dest/${name}-sphinx-$theme
-cp -r css $name.html ._$name*.html $name2.pdf $dest
+cp -r $name.html ._$name*.html $name2.pdf $dest
 cp -r sphinx-rootdir-$theme/_build/html $dest/${name}-sphinx-$theme
 
 
